b3422f0b12f30b9e5643e16e7dabb3bcc6fd554b
Obsydian-Falcon <walk.dauphin@gmail.com>
2025-05-13 12:29:17 -0600

    Async data

---
+++ hacker-stories/src/App_async_data.jsx
@@ -0,0 +1,171 @@
+import * as React from 'react';
+
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+// This here is a function that will return a promise with data once it is resolved
+// The resloved object will hold the previous list of stories.
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
+
+  // Set the stories to empty
+  const [stories, setStories] = React.useState([]);
+
+  // Call the async function and return the promise as a side-effect
+  // With the empty dependency array, this runs once the component mounts, once.
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    </div>
+  );
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+const List = ({ list, onRemoveItem }) => (
+  <ul>
+    {list.map((item) => (
+      <Item
+        key={item.objectID}
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
+    ))}
+  </ul>
+);
+
+const Item = ({ item, onRemoveItem }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
+
+export default App;
+
+//----- REACT ASYNC DATA NOTES ----
+// Here, we are going to simulate receiving data from a remote endpoint,
+// to do so, we will initialize our stories' state as empty.
+//
+// So first, we set up an async promise, 
+// then we set the stories to an empty array, 
+// then we have a useEffect with an empty dependency array to return the promise.
+//
+// So, what happens is that the page loads and since there's an empty array, nothing is rendered.
+// After the App component is rendered, the side-effect hook runs ONCE to get the async data.
+// After the promise is resolved and the data is set in the component's state, the component
+// renders again and shows the list.
+//----- REACT ASYNC DATA NOTES END ----
